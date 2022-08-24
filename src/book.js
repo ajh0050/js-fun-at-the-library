d674f0908e11de4ce9a5e688f6b6235052a8ce9b
Adam Hughes <adamjordanhughes@gmail.com>
2022-08-24 18:59:04 -0400

    Add first half of the book test

--- src/book.js
+++ src/book.js
@@ -1,11 +1,28 @@
-function createTitle() {
-  
-}
+function createTitle(title) {
+  return `The ${title}`
+};
+
+function buildMainCharacter(name,age,pronouns){
+  var character = {
+  'name': name,
+  'age': age,
+  'pronouns': pronouns,
+  };
+  return   character
+};
+
+function saveReview(review){
+var reviews = [review];
+// reviews.push(review);
+// return reviews.length;
+return reviews;
+};
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
