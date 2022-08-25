e192748cdf9326f11d335b6ae1385f1f7858d130
Adam Hughes <adamjordanhughes@gmail.com>
2022-08-25 15:15:35 -0400

    Add the first half of the functions

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,27 @@
+function shelfBook(book,shelfLocation){
+if (shelfLocation.length < 3 ){
+  shelfLocation.unshift(book);
+};
+return shelfLocation;
+};
+
+function unshelfBook(book,shelfLocation) {
+
+for (let i=0; i < shelfLocation.length; i++ ){
+  if (shelfLocation[i].title == book){
+    shelfLocation.splice(i)
+  };
+};
+//   if (shelfLocation[0] == book) {
+//     shelfLocation.shift(book)
+//   } else
+// shelfLocation.filter(shelf => book.title != book);
+return shelfLocation
+};
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
