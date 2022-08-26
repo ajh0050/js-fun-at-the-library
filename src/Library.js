b0c220853d4d9ec94d4180b792f1f94c73ac41c8
Adam Hughes <adamjordanhughes@gmail.com>
2022-08-25 23:08:35 -0400

    Add the first half of Library functionality

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,20 @@
+function createLibrary(name){
+var library = {}
+library.name = name;
+library.shelves = {
+  'fantasy' :[],
+  'fiction' : [],
+  'nonFiction' :[],
+};
+return library;
+};
 
+function addBook(library,book) {
+  library.shelves.
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+  createLibrary,
+  addBook,
   // checkoutBook
 };
