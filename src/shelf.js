ddaf2f3ce69defc7b508b192ff087c55fcde23b4
Adam Hughes <adamjordanhughes@gmail.com>
2022-08-25 21:19:10 -0400

    Add the completed shelf functionality

--- src/shelf.js
+++ src/shelf.js
@@ -1,27 +1,40 @@
-function shelfBook(book,shelfLocation){
-if (shelfLocation.length < 3 ){
-  shelfLocation.unshift(book);
+function shelfBook(book,shelf){
+if (shelf.length < 3 ){
+  shelf.unshift(book);
 };
-return shelfLocation;
+return shelf;
 };
 
-function unshelfBook(book,shelfLocation) {
-
-for (let i=0; i < shelfLocation.length; i++ ){
-  if (shelfLocation[i].title == book){
-    shelfLocation.splice(i)
+function unshelfBook(book,shelf) {
+for (let i=0; i < shelf.length; i++ ){
+  if (shelf[i].title == book){
+    shelf.splice(i,1)
   };
 };
-//   if (shelfLocation[0] == book) {
-//     shelfLocation.shift(book)
-//   } else
-// shelfLocation.filter(shelf => book.title != book);
-return shelfLocation
+return shelf
 };
 
+function listTitles(shelf){
+  var arrayOfTitles = [];
+  for (let i=0; i < shelf.length; i++ ){
+  arrayOfTitles.push(shelf[i].title)
+    };
+  titles = `${arrayOfTitles[0]}, ${arrayOfTitles[1]}, ${arrayOfTitles[2]}`;
+  return titles;
+};
+
+function searchShelf(shelf,book){
+onShelf = false ;
+for (let i=0; i < shelf.length; i++ ){
+  if (shelf[i].title == book){
+    onShelf = true;
+  };
+};
+return onShelf
+};
 module.exports = {
   shelfBook,
   unshelfBook,
-  // listTitles,
-  // searchShelf
+  listTitles,
+  searchShelf
 };
